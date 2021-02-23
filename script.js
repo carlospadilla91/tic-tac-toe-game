ddaff3cc362a0091fd41f9a0528ac224167c48c6
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-02-22 22:18:30 -0600

    feat: css done, need functionality

--- script.js
+++ script.js
@@ -3,22 +3,4 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
 console.log(boxes);
 
-const drawBoard = () => {
-    boxes.forEach((box, index) => {
-        let styleString = ""
-        if (index < 3) {
-            styleString += `border-bottom: 3px solid white;`;
-        }
-        if (index % 3 === 2) {
-            styleString += `border-left: 3px solid white;`;
-        }
-        if (index % 3 === 0) {
-            styleString += `border-right: 3px solid white;`;
-        }
-        if (index > 5) {
-            styleString += `border-top: 3px solid white;`;
-        }
-        box.style = styleString;
-    });
-}
-drawBoard();
+
